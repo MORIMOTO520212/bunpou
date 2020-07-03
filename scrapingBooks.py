0d7b4f7b59bed3217e9725fefe11bb828fbb80b0
MORIMOTO520212 <kanten520212@gmail.com>
2020-07-04 03:10:53 +0900

    2020/07/04

---
+++ scrapingBooks.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests, json, re, setting
+
+historyPath, BunpouPath = setting.set(0)
+
+i = 1
+while True:
+    html = requests.get('http://www.gutenberg.org/ebooks/{}'.format(i))
+    soup = BeautifulSoup(html.text, 'html.parser')
+    title = soup.select('#content > div.header > h1')
+    title = title[0]
+    title = re.split('[<>]', str(title))
+    print(title[2])
+
+    url = 'http://www.gutenberg.org/ebooks/{}.txt.utf-8'.format(str(i))
+
+    with open(historyPath, 'r') as f:
+        _d = json.load(f)
+    jsonData = {}
+    jsonData = _d
+    if title[2] not in jsonData:
+        jsonData[title[2]] = {
+            "status": False,
+            "url": url
+        }
+        with open(historyPath, 'w') as f:
+            json.dump(jsonData, f, indent=4)
+
+    i += 1
