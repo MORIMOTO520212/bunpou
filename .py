dbcfab711004f8a54d03821e62f170f1f06e102e
MORIMOTO520212 <kanten520212@gmail.com>
2020-08-03 15:06:50 +0900

    2020/08/03

--- .py
+++ .py
@@ -1,2 +1,14 @@
-with open('.syncprimitive', 'r') as f:
-    print(f.read())
+import psutil
+
+for proc in psutil.process_iter():
+       try:
+           flist = proc.get_open_files()
+           if flist:
+               print(proc.pid,proc.name)
+               for nt in flist:
+                   print("\t",nt.path)
+
+       # This catches a race condition where a process ends
+       # before we can examine its files    
+       except psutil.NoSuchProcess as err:
+           print("****",err) 
